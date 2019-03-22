85241f4aa0cb9aa4562ca2786d4d675c6a1424e6
jose manuel alfonso <jose.manuelchema111@gmail.com>
2019-03-22 17:33:03 +0100

    Plantilla base

---
+++ db/db.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS faltasdeasistencia;
+CREATE DATABASE IF NOT EXISTS faltasdeasistencia;
+
+USE faltasdeasistencia;
+
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE IF NOT EXISTS usuarios(
+    id        int(11) auto_increment,
+    nickname  varchar(150),
+    nombre    varchar(150),
+    apellidos varchar(150),
+    email     varchar(200),
+    password  varchar(255),
+    role      int(1) DEFAULT 1,
+    img       varchar(255),
+CONSTRAINT pk_usuarios PRIMARY KEY (id)
+)Engine=InnoDB DEFAULT CHARSET = latin1;
+
+DROP TABLE IF EXISTS alumnos;
+CREATE TABLE IF NOT EXISTS alumnos(
+    id        int(11) auto_increment,
+    matricula int(11),
+    nombre    varchar(150),
+    apellidos varchar(150),
+    img       varchar(255),
+CONSTRAINT pk_alumnos PRIMARY KEY (id)
+)Engine=InnoDB DEFAULT CHARSET = latin1;
+
+DROP TABLE IF EXISTS asignaturas;
+CREATE TABLE IF NOT EXISTS asignaturas(
+    id        int(11) auto_increment,
+    codigo    int(11),
+    nombre    varchar(150),
+    horas     int(4),
+    curso     varchar(10),
+    img       varchar(255),
+CONSTRAINT pk_asignaturas PRIMARY KEY (id)
+)Engine=InnoDB DEFAULT CHARSET = latin1;
+
+DROP TABLE IF EXISTS faltas;
+CREATE TABLE IF NOT EXISTS faltas(
+    alumno_id       int(11),
+    asignatura_id   int(11),
+    dia             DATE,
+    horas           int(4),
+CONSTRAINT pk_alumnos PRIMARY KEY (alumno_id,asignatura_id, dia),
+CONSTRAINT fk_faltas_alumnos FOREIGN KEY (alumno_id) REFERENCES alumnos(id),
+CONSTRAINT fk_faltas_asignaturas FOREIGN KEY (asignatura_id) REFERENCES asignaturas(id)
+)Engine=InnoDB DEFAULT CHARSET = latin1;
---
+++ index.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+     <!-- Local -->
+    <link rel="stylesheet" href="./assets/css/mainStyles.css" type="text/css">
+
+     <!-- Font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Faltas de asistencia</title>
+</head>
+<body>
+    
+    <!-- Main container -->
+    <div class="container-fluid blackBg vh-100">
+        <div class="row">
+
+            <!-- 1st Big col -->
+            <div class="col-1 text-white text-center blackDarkBg py-3 vh-100">
+                <p class="fas fa-star display-4 text-warning"></p>
+
+                <!-- favsList -->
+                <div class="row">
+                    <div class="col-12 py-2">
+                        <div class="circulo">
+                            <a href="">
+                                <img src="https://picsum.photos/200/200?image=3" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 py-2">
+                        <div class="circulo">
+                            <a href="">
+                                <img src="https://picsum.photos/200/200?image=4" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 py-2">
+                        <div class="circulo">
+                            <a href="">
+                                <img src="https://picsum.photos/200/200/?random?image=2" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div><!-- End 1st Big col -->
+
+            <!-- 2nd Big col -->
+            <div class="col-2 text-white text-center sidebar py-3  vh-100">
+
+                <!-- Logo -->
+                <div class="row">
+                    <div id="favLogoText" class="col-12">
+                        <div class="circulo circuloGrande">
+                            <a href="">
+                                <img src="https://picsum.photos/200/200?image=8" alt="">
+                            </a>
+                        </div>
+                            <p class="mt-2">Chema Alfonso</p>
+                    </div>
+                </div>
+
+                <!-- Enlaces principales -->
+                <div class="row">
+                    <div class="col-12 mainLinksContainer">
+                        <ul>
+                            <li><a href="#">INICIO</a></li>
+                            <li><a href="#">FALTAS</a></li>
+                            <li><a href="#">ALUMNOS</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                
+            </div><!-- End 2nd Big col -->
+
+            <!-- 3rd Big col -->
+            <div class="col-9 blackBg mainBody py-3  vh-100">
+
+                <!-- Big container 3 -->
+                <div class="container-fluid">
+
+                <!-- Breadcrums -->
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="breadcrumb text-white">
+                            <a class="breadcrumb-item" href="#">Pagina 1</a>
+                            <a class="breadcrumb-item" href="#">Pagina 2</a>
+                            <span class="breadcrumb-item active"></span>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Main title -->
+                <div class="row align-items-center">
+                    <header class="col-11 m-auto bg-white text-center pt-5">
+                            <h1 id="mainTitle" class="display-4">Faltas de asistencia</h1>
+                            <p id="mainTitle" class="text-left pt-5">Resumen general</p>
+                    </header>
+                </div>
+
+                <!-- Pages -->
+
+                <!-- Cards Row -->
+                <div class="row">
+                    <div class="col-11 m-auto bg-white text-center">
+
+                        <!-- Cards Container -->
+                        <div class="container">
+
+                            <!-- 1st 1st card -->
+                            <div class="row mb-3">
+
+                                <div class="col-6 card">
+
+                                    <!-- Texto y foto principal -->
+                                    <div class="row h-50">
+
+                                        <div class="col-6">
+                                            <div class=" text-left p-2">
+                                                <h5>Top Total</h5>
+                                                <p>Nombre: </p>
+                                                <p>Apellidos: </p>
+                                                <p>Faltas: </p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-6">
+                                            <div class="circulo circuloExtraGrande mt-4">
+                                                <img src="https://picsum.photos/200/200?image=22" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <!-- miniaturas -->
+                                    <div class="row justify-content-center mt-5">
+
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=8" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=2" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=5" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=3" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=11" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- End card -->
+
+                                <!-- 2nd 1st card -->
+                                <div class="col-6 card">
+
+                                    <!-- Texto y foto principal -->
+                                    <div class="row h-50">
+
+                                        <div class="col-6">
+                                            <div class=" text-left p-2">
+                                                <h5>Top Alumnos</h5>
+                                                <p>Nombre: </p>
+                                                <p>Apellidos: </p>
+                                                <p>Faltas: </p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-6">
+                                            <div class="circulo circuloExtraGrande mt-4">
+                                                <img src="https://picsum.photos/200/200?image=16" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <!-- miniaturas -->
+                                    <div class="row justify-content-center mt-5">
+
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=8" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=2" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=5" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=3" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=11" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- End card -->
+                                
+                            </div>
+
+                            <!-- 1st 2nd card -->
+                            <div class="row mb-3">
+
+                                <div class="col-6 card">
+
+                                    <!-- Texto y foto principal -->
+                                    <div class="row h-50">
+
+                                        <div class="col-6">
+                                            <div class=" text-left p-2">
+                                                <h5>Top Asignaturas</h5>
+                                                <p>Nombre: </p>
+                                                <p>Horas: </p>
+                                                <p>Faltas: </p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-6">
+                                            <div class="circulo circuloExtraGrande mt-4">
+                                                <img src="https://picsum.photos/200/200?image=24" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <!-- miniaturas -->
+                                    <div class="row justify-content-center mt-5">
+
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=8" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=2" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=5" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=3" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=11" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- End card -->
+
+                                <!-- 2nd 2nd card -->
+                                <div class="col-6 card">
+
+                                    <!-- Texto y foto principal -->
+                                    <div class="row h-50">
+
+                                        <div class="col-6">
+                                            <div class=" text-left p-2">
+                                                <h5>Top Total</h5>
+                                                <p>Nombre: </p>
+                                                <p>Apellidos: </p>
+                                                <p>Faltas: </p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-6">
+                                            <div class="circulo circuloExtraGrande mt-4">
+                                                <img src="https://picsum.photos/200/200?image=26" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <!-- miniaturas -->
+                                    <div class="row justify-content-center mt-5">
+
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=8" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=2" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=5" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=3" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-2">
+                                            <div class="circulo circuloGrande">
+                                                <img src="https://picsum.photos/200/200?image=11" alt="">
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <!-- End card -->
+
+                            </div><!-- End 2nd card row -->
+
+                        </div><!-- End cards container -->
+                    </div><!-- End col Cards -->
+                </div><!-- End row Cards -->
+
+                
+                <!-- End Pages -->
+
+
+            </div><!-- End big container 3 -->
+        </div><!-- End big col 3 -->
+
+    </div><!-- End main row -->
+
+</div><!-- End main container -->
+
+
+
+
+
+ <!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
